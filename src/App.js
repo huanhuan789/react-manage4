393d10f356c51325ac2faada3d0b14fffbcbe4c3
huanhuan789 <liuhuan.luck@qq.com>
2020-09-11 09:15:49 +0800

    9.11登录页面

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import React from 'react';
 // import { Button } from 'antd';
 import './App.less';
-import Login from './pages/login/Login'
+import Login from './pages/login/Login';
+import Admin from './pages/admin/Admin';
+import {HashRouter, Switch, Route} from 'react-router-dom'
 function App() {
   return (
-<Login/>
+    <HashRouter>
+      <Switch>{/* 只匹配其中一个路由 */}
+        <Route path='/' component={Login} />
+        <Route path='/admin' component={Admin} />
+      </Switch>
+
+    </HashRouter>
   );
 }
 
---
+++ src/pages/admin/Admin.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+// 后台管理的路由组件
+function Admin(){
+    return(
+        <div>
+            Admin
+        </div>
+    )
+}
+export default Admin
